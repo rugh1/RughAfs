0e33589ee1e721563ed22950ec13c830c8550675
rugh1 <royigloberman@gmail.com>
2025-04-26 17:25:15 +0300

    fixed encryption for non bytes type

--- kerberos/base/encryptions.py
+++ kerberos/base/encryptions.py
@@ -1,3 +1,4 @@
+import pickle
 from cryptography.fernet import Fernet
 
 class encr:
@@ -5,6 +6,8 @@ class encr:
     def encrypt(value, key):
         if value is None:
             return None 
+        if type(value) != bytes:
+            value = pickle.dumps(value)
         cipher_suite = Fernet(key)
         return cipher_suite.encrypt(value)
         #idea encrpyt user name but give in ticket /first iteraction random number that represents the user
